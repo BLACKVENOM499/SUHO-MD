dceb874fccbb41f45dcc6413d34329239165640d
BLACKVENOM499 <blackvenomx499@gmail.com>
2026-01-28 13:50:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "7MNgmaRA#aNI08Vi8_k-9Cdsx5REWmIb-hGudsGGBDGfiaZxBxPY",
   OWNER_NUM: process.env.OWNER_NUM || "94764703165",
   OWNER_NAME: process.env.OWNER_NAME || "Akindu",
-  REPO: process.env.REPO || "https://github.com/altzzdevs/SUHO-MD", 
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
