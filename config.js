9db1e35b38cf733d23b2847bbd8bec21e6931b0e
BLACKVENOM499 <blackvenomx499@gmail.com>
2026-01-28 13:48:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
-  OWNER_NUM: process.env.OWNER_NUM || "27649342626",
-  OWNER_NAME: process.env.OWNER_NAME || "Lord Sung",
+  SESSION_ID: process.env.SESSION_ID || "7MNgmaRA#aNI08Vi8_k-9Cdsx5REWmIb-hGudsGGBDGfiaZxBxPY",
+  OWNER_NUM: process.env.OWNER_NUM || "94764703165",
+  OWNER_NAME: process.env.OWNER_NAME || "Akindu",
   REPO: process.env.REPO || "https://github.com/altzzdevs/SUHO-MD", 
   PREFIX: process.env.PREFIX || ".",
-  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
   MODE : process.env.MODE || "public", 
-  AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
+  AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "false", 
   AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "false"), 
   ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
   version: process.env.version || "V2",
